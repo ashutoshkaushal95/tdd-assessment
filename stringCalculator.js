81c57248aacc4e67197861e367688901dd447bd6
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2024-10-26 12:10:19 +0530

    5th test check if the function remove the new line syntax from the given string and returns the correct output

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,6 @@
 const add = (numbers) => {
   if (numbers === "") return 0;
-  const numArray = numbers.split(",").map(Number);
+  const numArray = numbers.split(/,|\n/).map(Number); // comma or newline
   return numArray.reduce((sum, num) => sum + num, 0);
 };
 
