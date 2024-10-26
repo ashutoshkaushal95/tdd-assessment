ebefcfb5fefc209eac6e1ee89475426ce1610d60
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2024-10-26 12:54:46 +0530

    added console logs for the step by step explanation of how the function operates.

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,14 +1,27 @@
 const add = (numbers) => {
+  console.log("numbers input: ", numbers);
+
   if (numbers === "") return 0;
 
+  // check for custom delimeters
   let delimiter = /,|\n/;
   if (numbers.startsWith("//")) {
     const parts = numbers.split("\n");
+
+    console.log("parts: ", parts);
+
     delimiter = new RegExp(parts[0].slice(2));
-    numbers = parts.slice(1).join("\n");
+
+    console.log("delimiter: ", delimiter);
+
+    numbers = parts.slice(1).join();
+
+    console.log("numbers converted: ", numbers);
   }
 
   const numArray = numbers.split(delimiter).map(Number);
+
+  console.log("numArray: ", numArray);
 
   // Check for negative numbers
   const negatives = numArray.filter((num) => num < 0);
@@ -18,5 +31,5 @@ const add = (numbers) => {
 
   return numArray.reduce((sum, num) => sum + num, 0);
 };
-
+console.log("answer", add("//;\n1;2;4;5"));
 module.exports = add;
